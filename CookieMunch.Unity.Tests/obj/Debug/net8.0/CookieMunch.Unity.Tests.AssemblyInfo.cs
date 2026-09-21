119c09f5de0a2eacb78e1a60f6ca03440ad6a9a0
Cookie Munch <noreply@cookiemunch.net>
2026-09-21 16:04:22 -0400

    sync from the product repo (6bd62cf)

--- CookieMunch.Unity.Tests/obj/Debug/net8.0/CookieMunch.Unity.Tests.AssemblyInfo.cs
+++ CookieMunch.Unity.Tests/obj/Debug/net8.0/CookieMunch.Unity.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookieMunch.Unity.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+667660f093c77c751cf675d103b15b12da78be10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+405ee84345fc8fe52c148d87239dceca9dad48f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookieMunch.Unity.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookieMunch.Unity.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
